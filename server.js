d3d1a805897c7186d67ff321bb551a1df4023764
Alex Martinez <alex@Alexs-MacBook-Air.local>
2023-10-17 15:07:51 -0400

    routes made

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const goalsRoute = require("./routes/goalsRoute");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use("/api/goals", goalsRoute);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));

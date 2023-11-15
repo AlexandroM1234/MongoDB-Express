311aa003e61a2178b01eb156806ed17dbd3660b4
Alex Martinez <alex@Alexs-MacBook-Air.local>
2023-11-15 10:53:39 -0500

    user model and route skeleton made

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const goalsRoute = require("./routes/goalsRoute");
+const userRoute = require("./routes/userRoute");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalsRoute);
+app.use("/api/users", userRoute);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
